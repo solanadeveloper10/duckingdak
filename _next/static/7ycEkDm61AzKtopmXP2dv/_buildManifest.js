0310b28d33d5071af9e170f34a639c6c0f178173
Solana Dev10 <solanadeveloper10@gmail.com>
2025-02-21 12:50:53 +0200

    init

---
+++ _next/static/7ycEkDm61AzKtopmXP2dv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/295-01af636f278dc440.js","static/css/a5fac4e1a47ecd94.css","static/chunks/pages/index-70e04ef80f719957.js"],"/_error":["static/chunks/pages/_error-95043b19f41f9a58.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
